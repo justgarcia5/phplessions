eb95ce99ab42b41299718cc2886effd4613f4bf0
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-10-29 11:43:15 -0700

    everything working

---
+++ forms.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<html>
+  <body>
+  <form action="send.php" method="post">
+    First name: <input type="text" name="fullname"/>
+    Email: <input type="text" name="email"/>
+    <input type="submit"/>
+  </form>
+  <body>
+</html>
